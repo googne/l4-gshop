9d6d37b76b56d70d232d1938233604641dbaf6da
googne19894 <aryanraj19894@gmail.com>
2023-06-25 14:47:40 +0530

    GSP-51 footer company name updated

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer>
       <Container>
         <Row>
-          <Col className='text-center py-3'>Copyright &copy; Googne Infra</Col>
+          <Col className='text-center py-3'>Copyright &copy; Googne Inc.</Col>
         </Row>
       </Container>
     </footer>
