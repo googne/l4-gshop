c497782bfa57d04a2f80f509ba70fa3883205684
googne19894 <aryanraj19894@gmail.com>
2023-06-25 22:41:00 +0530

    GSP-62 user authentications

---
+++ backend/controllers/userController.js
@@ -0,0 +1,47 @@
+import asyncHandler from 'express-async-handler'
+import User from '../models/userModel.js'
+
+/**
+ * @desc Auth user & get token
+ * @route POST /api/users/login
+ * @access Public
+ */
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body
+  const user = await User.findOne({ email })
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: null,
+    })
+  } else {
+    res.status(401)
+    throw new Error('Invalid email or password')
+  }
+
+  res.send({
+    email,
+    password,
+  })
+})
+
+/**
+ * @desc Fetch single product
+ * @route GET /api/products/:id
+ * @access Public
+ */
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id)
+  if (product) {
+    res.json(product)
+  } else {
+    res.status(404)
+    throw new Error('Product not found')
+  }
+})
+
+export { authUser }
