3bc2454e6a8be7a0275b6541ec5f543483932c1b
googne19894 <aryanraj19894@gmail.com>
2023-07-02 18:00:44 +0530

    GSP-71: Order API

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
 
 dotenv.config()
 
@@ -20,8 +21,9 @@ app.get('/', (req, res) => {
   res.send('API is running.......')
 })
 
-app.use('/api/products/', productRoutes)
-app.use('/api/users/', userRoutes)
+app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
 
 //custom error handler
 app.use(notFound)
