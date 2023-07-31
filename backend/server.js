88b4c7c7a64685746a469fadf233c8fe3e4dda24
googne19894 <aryanraj19894@gmail.com>
2023-07-31 16:00:21 +0530

    GSP-85: production build deloyment

--- backend/server.js
+++ backend/server.js
@@ -24,10 +24,6 @@ if (process.env.NODE_ENV === 'development') {
 // it will allow to accept json format to its body
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('API is running.......')
-})
-
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
@@ -41,6 +37,19 @@ app.get('/api/config/paypal', (req, res) =>
 //making upload folder static
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+//production configuration
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running.......')
+  })
+}
 
 //custom error handler
 app.use(notFound)
